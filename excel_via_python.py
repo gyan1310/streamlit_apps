cc19672c19c2b2dabcf339183b3a6fedb9f7e29c
Gyanchand Sharma <96147148+gyan1310@users.noreply.github.com>
2023-09-29 19:11:10 +0530

    Update excel_via_python.py

--- excel_via_python.py
+++ excel_via_python.py
@@ -3,10 +3,6 @@ import pandas as pd
 
 st.title("Excel Data Filters App")
 
-# Input fields for user-defined values
-column_to_filter = st.text_input("Enter the column to filter:")
-value_to_replace = st.text_input("Enter the value to replace:")
-
 # File upload widget
 uploaded_file = st.sidebar.file_uploader("Upload an Excel file")
 
@@ -14,6 +10,14 @@ uploaded_file = st.sidebar.file_uploader("Upload an Excel file")
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     
+    # Get a list of column names from the DataFrame
+    columns_list = df.columns.tolist()
+    
+    # Create a selectbox widget for selecting the column
+    column_to_filter = st.selectbox("Select a column to filter:", columns_list)
+
+    value_to_replace = st.text_input("Enter the value to replace:")
+
     def remove_none_values(df, column):
         df = df.dropna(subset=[column])
         return df
@@ -31,14 +35,14 @@ if uploaded_file is not None:
         if column_to_filter:
             df = remove_none_values(df, column_to_filter)
         else:
-            st.warning("Please enter a valid column name.")
+            st.warning("Please select a valid column.")
 
     if st.button("Replace Values"):
         if column_to_filter and value_to_replace:
             new_value = st.text_input("Enter the new value:")
             df = replace_values(df, column_to_filter, value_to_replace, new_value)
         else:
-            st.warning("Please enter valid column and value.")
+            st.warning("Please select a valid column and enter a value to replace.")
 
     if st.button("Find Min and Max"):
         if column_to_filter:
@@ -46,7 +50,7 @@ if uploaded_file is not None:
             st.write(f"Minimum Value: {min_val}")
             st.write(f"Maximum Value: {max_val}")
         else:
-            st.warning("Please enter a valid column name.")
+            st.warning("Please select a valid column.")
             
     if df is not None:
         st.write("Filtered Data:")
